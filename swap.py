02ee78e74335307db547f10e389b23d886998cb9
Subha7 <33711869+bootkernel@users.noreply.github.com>
2018-11-10 19:20:57 -0800

    Update swap.py

--- swap.py
+++ swap.py
@@ -2,12 +2,16 @@
 x = 1
 y = 0
 
-print("Original Values (x, y)", x, y)
+print("Original Values (x, y):", x, y)
 
 #Using XOR to swap values instead of using a temporary variable
 x ^= y
 y ^= x
 x ^= y
 
-print("Swapped Values (x, y)", x, y)
+print("Swapped Values (x, y):", x, y)
+
+# Output:
+# Original Values (x, y): 1 0
+# Swapped Values (x, y): 0 1
 
