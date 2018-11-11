1223926a82597e1bdb34823f1bb8d8d67c360568
Subha7 <33711869+bootkernel@users.noreply.github.com>
2018-11-10 18:30:43 -0800

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,22 @@
+# Crypto Function
+def crypto(string):
+   # Using ASCII Table
+   midpoint_float = 109.5
+   decrypted = ""
+   for char in string:
+      if 97 <= ord(char) <= 122:                         # 97 to 122 is A - Z
+                                                         # in Unicode
+
+         calc = float(midpoint_float) - ord(char)        # ord() returns an integer which
+                                                         # represents an unicode character
+
+         char = midpoint_float + calc
+         decrypted = decrypted + chr(int(char))
+      else:
+         decrypted = decrypted + char
+   return decrypted
+
+# Testing
+string = "z xzg"
+print("\nCryptic Message:", string)
+print("\nDecrypted Message: ", crypto(string))
