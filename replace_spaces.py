2972adfca7d0dde18f2613d08de058ac330490af
Subhajeet Mukherjee <33711869+bootkernel@users.noreply.github.com>
2018-11-12 22:14:37 -0800

    Update replace_spaces.py

--- replace_spaces.py
+++ replace_spaces.py
@@ -18,4 +18,28 @@ print("\nHere's the given string with the spaces replaced:", replace_spaces(str)
 #
 # Here's the given string with the spaces replaced: Hello,%20I%20am%20bootkernel!
 
-# Second Solution:
+# Second Credible Solution:
+# Defining the credible "Replace spaces" Function
+def replace_spaces_credible(str):
+   replace = ''                              # Blank variable that holds nothing
+   for c in str:                             # A loop to traverse through the
+                                             # given string
+
+      if (c == ' '):                         # if c/char is 'white_space',
+         replace = replace + '%20'           # replace the whitespace with %20
+
+      else:                                  # Else,
+         replace = replace + c               # replace the whitespace with c
+   return replace
+
+# Testing with a string
+str = "Hello, I am bootkernel!"
+
+print("Given String:", str)                  # Prints given string
+
+print("\nHere's the given string with the spaces replaced:", replace_spaces_credible(str))
+
+# Output:
+# Given String: Hello, I am bootkernel!
+#
+# Here's the given string with the spaces replaced: Hello,%20I%20am%20bootkernel!
