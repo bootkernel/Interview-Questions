c8049f6f87d213dff0eb4929659acd04078f9dfa
Subhajeet Mukherjee <33711869+bootkernel@users.noreply.github.com>
2018-11-12 16:22:04 -0800

    Add files via upload

---
+++ first_recur.py
@@ -0,0 +1,22 @@
+# First Recurring Character in a Given String
+# Defining the Function
+def recur(str):
+   num = {}                            # Creating a Dictionary
+
+   for c in str:                       # A loop to traverse through the given string
+
+      if c in num:                     # If we find 'c' in the num dictionary,
+         return c                      # we'll return it
+      else:                            # Else,
+         num[c] = 0                    # we'll add the character to num
+
+   print("No Recurring Character!")    # and Print that there's no first
+                                       # recurring character
+
+# Testing with a string
+str = "The Programming Foundation"
+# Printing the first recurring character
+print("\nThe First Recurring Character is: ", recur(str))
+
+# Output:
+# The First Recurring Character is:  r
